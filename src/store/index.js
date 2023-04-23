cf74228a6df7adc5803fb1295a3030e513e4908b
Iago-pixel <pristonvinicius@hotmail.com>
2023-04-23 20:37:28 -0300

    feat[store]: add index with initil configs

---
+++ src/store/index.js
@@ -0,0 +1,9 @@
+import { createStore, combineReducers, applyMiddleware } from "redux";
+
+import ReduxThunk from "redux-thunk";
+
+const reducers = combineReducers({});
+
+const store = createStore(reducers, applyMiddleware(ReduxThunk));
+
+export default store;
